576a7be79d8ece1e9c346878e9201ff6ffe8388b
anfal911 <anfuuuvaktapurya@gmail.com>
2023-01-04 16:11:10 +0530

    third init

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const app = express();
+const http = require("http");
 const ejs = require("ejs");
 // const expressEjsLayout = require("express-ejs-layouts");
 const path = require("path");
+const nodemailer = require("nodemailer");
 const expressEjsLayouts = require("express-ejs-layouts");
+const { json } = require("express");
 const PORT = process.env.PORT || 3000;
+
+var server = http.Server(app);
+app.use(express.json());
 
 app.use(express.static("public"));
 
 require("./routes/webRoute")(app);
-require("./routes/serviceRoute")(app);
+require("./routes/serviceRoute")(app, nodemailer);
 require("./routes/libraryRoute")(app);
-
-require("./public/assets/js/sendmail");
 
 // set template engine
 app.use(expressEjsLayouts);
